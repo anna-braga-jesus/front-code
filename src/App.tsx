e89ac3929fd450a7ec9ec607cdcc956fe9f42a6d
anna-braga-jesus <annajesus.nave@gmail.com>
2023-05-09 20:28:54 -0300

    feat: add personal menu scree

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import './App.css'
 import { Card } from './components/card/card';
 import { useFoodData } from './hooks/useFoodData';
+import { CreateModal } from './components/create-modal/create-modal';
 
 function App() {
   const {data} = useFoodData();
+  const [isModalOpen, setIsModalOpen]= useState(false);
+
+const handleOpenModal = () => {
+setIsModalOpen(prev => !prev)
+}
 
   return (
     <div className='container'>
-      <h1>Cardápio Rango</h1>
+      
+      <h1>Cardápio</h1>
       <div className="card-grid">
     {data?.map(foodData => 
     <Card 
@@ -17,6 +24,8 @@ function App() {
     image={foodData.image}
     />)}
       </div>
+      {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}    
+      <button onClick={handleOpenModal}>novo</button>
     </div>
   )
 }
